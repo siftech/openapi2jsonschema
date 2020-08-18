1889c1e230283008158d1b9b6daff4abd8e120ac
Jesse Roland <jroland@sift.net>
2020-08-18 08:32:36 -0500

    Added files necessary for pip installation

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+setup(
+    name="openapi2jsonschema",
+    version="1.0.1",
+    description="Converts openAPI YAML into JSON. Modified from https://github.com/instrumenta/openapi2jsonschema",
+    author="",
+    author_email="",
+    packages=["openapi2jsonschema"],  # same as name
+    install_requires=[
+        "click==7.0",
+        "colorama==0.4.1",
+        "jsonref==0.2",
+        "pyyaml==5.1",
+    ],  # external packages as dependencies
+    scripts=[],
+)
